38a525599493ba7980e4b36893e52c5316608052
r10945018 <rime97410000@gmail.com>
2022-05-17 19:42:49 +0800

    turn off no function code

--- T5_small/train.py
+++ T5_small/train.py
@@ -115,33 +115,33 @@ if __name__ == "__main__":
     data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, label_pad_token_id=-100)
     metric = load_metric("sacrebleu")
 
-    def postprocess_text(preds, labels):
-        preds = [pred.strip() for pred in preds]
-        labels = [[label.strip()] for label in labels]
+    # def postprocess_text(preds, labels):
+    #     preds = [pred.strip() for pred in preds]
+    #     labels = [[label.strip()] for label in labels]
 
-        return preds, labels
+    #     return preds, labels
 
-    def compute_metrics(eval_preds):
-        preds, labels = eval_preds
-        if isinstance(preds, tuple):
-            preds = preds[0]
-        print(preds[0])
-        decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-        if args.ignore_pad_token_for_loss:
-            # Replace -100 in the labels as we can't decode them.
-            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    # def compute_metrics(eval_preds):
+    #     preds, labels = eval_preds
+    #     if isinstance(preds, tuple):
+    #         preds = preds[0]
+    #     print(preds[0])
+    #     decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    #     if args.ignore_pad_token_for_loss:
+    #         # Replace -100 in the labels as we can't decode them.
+    #         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    #     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-        # Some simple post-processing
-        decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+    #     # Some simple post-processing
+    #     decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
 
-        result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-        result = {"bleu": result["score"]}
+    #     result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    #     result = {"bleu": result["score"]}
 
-        prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
-        result["gen_len"] = np.mean(prediction_lens)
-        result = {k: round(v, 4) for k, v in result.items()}
-        return result
+    #     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    #     result["gen_len"] = np.mean(prediction_lens)
+    #     result = {k: round(v, 4) for k, v in result.items()}
+    #     return result
 
     # Train model
     training_args = Seq2SeqTrainingArguments(
