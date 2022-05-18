24f2c609041c7689a6777b4330abb0e2f8d9292b
DANTA-HOJA <rime97410000@gmail.com>
2022-05-18 20:19:54 +0800

    add gpt-2 "vocab.json"（from huggingface model hub）

--- Multigen/download_pre_trained_GPT_2.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p models
-cd models
-mkdir -p gpt2-small
-wget -O pytorch_model.bin https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin
-wget -O vocab.json https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
-wget -O merges.txt https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
-wget -O config.json https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-config.json
